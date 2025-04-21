39d377944e454fb3293a04058e2aebb702073812
Timofei Verkhososov <148647986+21Timofei@users.noreply.github.com>
2025-04-21 15:15:00 +0300

    Changed files

--- app/util/rand.go
+++ app/util/rand.go
@@ -3,12 +3,9 @@ package util
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
+//removed inti() becouse from go 1.20 rand generator initialise automaticly
 
 func GenerateID() string {
 	hi := rand.Uint64()
